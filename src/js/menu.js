5e3aedc954aaf4a6e95ba5799264a09d7cb14fb4
tymur_lykho <tima.likho@gmail.com>
2024-12-26 10:04:04 +0200

    Update hover for other-links, add function for closing menu after click in link, add rel & title for social-links

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,12 +1,25 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-menu-open]"),
-    closeModalBtn: document.querySelector("[data-menu-close]"),
+    closeModalBtn: document.querySelectorAll("[data-menu-close]"),
     modal: document.querySelector("[data-menu]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+  refs.closeModalBtn.forEach(btn => {
+    btn.addEventListener("click", event => {
+      // Якщо це <a>, перевірити наявність href
+      if (btn.tagName === "A" && btn.hasAttribute("href")) {
+        // Дозволяємо стандартний перехід
+        toggleModal();
+      } else {
+        // Якщо це кнопка або елемент без href
+        event.preventDefault();
+        toggleModal();
+      }
+    });
+  });
 
   function toggleModal() {
     refs.modal.classList.toggle("is-open");
