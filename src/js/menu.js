a4b9a4861abfe0f3a8142300921506da52111ff4
tymur_lykho <tima.likho@gmail.com>
2024-12-26 10:28:00 +0200

    Add scroll-lock when mobile-menu is open

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,18 +3,16 @@
     openModalBtn: document.querySelector("[data-menu-open]"),
     closeModalBtn: document.querySelectorAll("[data-menu-close]"),
     modal: document.querySelector("[data-menu]"),
+    body: document.body,
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   
   refs.closeModalBtn.forEach(btn => {
     btn.addEventListener("click", event => {
-      // Якщо це <a>, перевірити наявність href
       if (btn.tagName === "A" && btn.hasAttribute("href")) {
-        // Дозволяємо стандартний перехід
         toggleModal();
       } else {
-        // Якщо це кнопка або елемент без href
         event.preventDefault();
         toggleModal();
       }
@@ -23,5 +21,6 @@
 
   function toggleModal() {
     refs.modal.classList.toggle("is-open");
+    refs.body.classList.toggle("scroll-lock");
   }
 })();
